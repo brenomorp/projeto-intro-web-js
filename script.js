4e7a4481641b310fc8f2bda57dab2b611223ee36
Breno Moreira <brenomoreirapro@gmail.com>
2022-11-11 20:25:11 -0300

    Arrays and Objects

---
+++ script.js
@@ -0,0 +1,106 @@
+const cursos = ["HTML e CSS", "JavaScript", "APIs REST"];
+
+const turmas = [
+    {
+        turma: "Hipátia",
+        curso: "JavaScript",
+        inicio: "30/11/2022",
+        termino: "30/01/2023",
+        numeroAlunos: "150",
+        periodo: "Noturno",
+        concluida: false
+    },
+    {
+        turma: "Sibyla",
+        curso: "JavaScript",
+        inicio: "30/10/2022",
+        termino: "30/12/2022",
+        numeroAlunos: "200",
+        periodo: "Integral",
+        concluida: false
+    },
+    {
+        turma: "Curie",
+        curso: "HTML e CSS",
+        inicio: "15/09/2022",
+        termino: "15/10/2022",
+        numeroAlunos: "180",
+        periodo: "Noturno",
+        concluida: true
+    },
+    {
+        turma: "Zhenyi",
+        curso: "HTML e CSS",
+        inicio: "01/11/2022",
+        termino: "01/01/2023",
+        numeroAlunos: "80",
+        periodo: "Integral",
+        concluida: false
+    },
+    {
+        turma: "Clarke",
+        curso: "HTML e CSS",
+        inicio: "04/07/2022",
+        termino: "04/09/2022",
+        numeroAlunos: "200",
+        periodo: "Noturno",
+        concluida: true
+    },
+    {
+        turma: "Blackwell",
+        curso: "APIsRest",
+        inicio: "20/03/2022",
+        termino: "20/06/2022",
+        numeroAlunos: "100",
+        periodo: "Integral",
+        concluida: true
+    },
+    {
+        turma: "Elion",
+        curso: "APIsRest",
+        inicio: "12/01/2022",
+        termino: "12/06/2022",
+        numeroAlunos: "200",
+        periodo: "Notruno",
+        concluida: true
+    },
+    {
+        turma: "Burnell",
+        curso: "APIsRest",
+        inicio: "18/10/2022",
+        termino: "18/04/2023",
+        numeroAlunos: "90",
+        periodo: "Integral",
+        concluida: false
+    },
+];
+
+const estudantes = [
+    {
+        estudante: "Chris Evans",
+        turma: "Hipátia",
+        curso: "JavaScript",
+        valor: "900",
+        nParcelas: 9,
+        desconto: false,
+        parcelas: 100
+    },
+    {
+        estudante: "Halle Berry",
+        turma: "Burnell",
+        curso: "APIsRest",
+        valor: "2000",
+        nParcelas: 4,
+        desconto: false,
+        parcelas: 500
+    },
+    {
+        estudante: "Lashana Lynch",
+        turma: "Zhenyi",
+        curso: "HTML e CSS",
+        valor: "500",
+        nParcelas: 1,
+        desconto: true,
+        parcelas: 500
+    }
+];
